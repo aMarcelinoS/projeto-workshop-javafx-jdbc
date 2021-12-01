53043e51a355b4dbb697181a2b0a106b35a059d8
Alexandre Marcelino <amsantos9@gmail.com>
2021-12-01 18:54:41 -0300

    Validation Exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -70,9 +73,18 @@ public class DepartmentFormController implements Initializable {
 		Utils.currentStage(event).close();
 		
 		}
+		catch(ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
 		catch(DbException e) {
 			Alerts.showAlert("Erro ao salvar o departamento", null, e.getMessage(), AlertType.ERROR);
 		}
+	}
+	
+	@FXML
+	private void onBtCancelAction(ActionEvent event) {
+		System.out.println("onBtCancelAction");
+		Utils.currentStage(event).close();
 	}
 	
 	private void notifyDataChangeListeners() {
@@ -85,20 +97,22 @@ public class DepartmentFormController implements Initializable {
 	private Department getFormData() {
 		Department obj = new Department();
 		
+		ValidationException exception = new ValidationException("Validation error");
+		
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		if(txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "Field can´t be empty");
+		}
 		obj.setName(txtName.getText());
+		
+		if (exception.getErrors().size() > 0){
+			throw exception;
+		}
 		
 		return obj;
 	}
 
-	@FXML
-	private void onBtCancelAction(ActionEvent event) {
-		System.out.println("onBtCancelAction");
-		Utils.currentStage(event).close();
-	}
-	
-	
-	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
@@ -116,4 +130,12 @@ public class DepartmentFormController implements Initializable {
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
 	}	
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if(fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
+	}
 }
